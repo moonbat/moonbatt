117bd1a083f0349fc585b380a544bae170a8e46a
moonbat <jymoon6691@hanmail.net>
2017-10-20 11:40:30 +0900

    first

---
+++ src/MyFirstOperator.java
@@ -0,0 +1,12 @@
+
+public class MyFirstOperator {
+ public static void main(String[] args) {
+	System.out.println(5+3);
+	System.out.println(5-3);
+	System.out.println(5*3);
+	System.out.println(5/3);
+	System.out.println(5%3);
+	 
+}	 
+ }
+
